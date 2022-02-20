9e59a844cd440edd9206bdc14dfad8aa0d546e4e
JJ Allaire <jj@rstudio.com>
2022-02-20 16:23:27 -0500

    additional language handlers for embedded completions

--- src/lsp/embedded.ts
+++ src/lsp/embedded.ts
@@ -113,6 +113,32 @@ function extensionForLanguage(language: string) {
   switch (language) {
     case "python":
       return "py";
+    case "ruby":
+      return "rb";
+    case "typescript":
+      return "ts";
+    case "javascript":
+      return "js";
+    case "html":
+      return "html";
+    case "sql":
+      return "sql";
+    case "r":
+      return "r";
+    case "css":
+      return "css";
+    case "cpp":
+    case "rcpp":
+      return "cpp";
+    case "go":
+      return "go";
+    case "java":
+      return "java";
+    case "latex":
+    case "tex":
+      return "tex";
+    case "rust":
+      return "rs";
     default:
       return language;
   }
