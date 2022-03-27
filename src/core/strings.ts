92e76b76fe4cc2398773c6992436e31146d02cb8
JJ Allaire <jj@rstudio.com>
2022-03-27 15:15:13 -0400

    windows shell escaping

--- src/core/strings.ts
+++ src/core/strings.ts
@@ -3,12 +3,16 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as os from "os";
+
 export function escapeRegExpCharacters(value: string): string {
   return value.replace(/[\\\{\}\*\+\?\|\^\$\.\[\]\(\)]/g, "\\$&");
 }
 
 export function shQuote(value: string): string {
-  if (/\s/g.test(value)) {
+  if (os.platform() === "win32") {
+    return value.replace(" ", "^ ");
+  } else if (/\s/g.test(value)) {
     return `"${value}"`;
   } else {
     return value;
